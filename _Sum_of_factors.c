dcac190289d64a356e53da441bcfbbdf2aec569b
Abhay Kamat <147469947+abhikamat2004@users.noreply.github.com>
2023-12-19 22:54:10 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int sum=0;     for(int i=1;i<a;i++){         if(a%i==0){             sum+=i;         }     }     printf("%d",sum); }

---
+++ _Sum_of_factors.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int sum=0;
+    for(int i=1;i<a;i++){
+        if(a%i==0){
+            sum+=i;
+        }
+    }
+    printf("%d",sum);
+}
