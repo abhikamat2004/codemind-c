b3689b1948be1a0adbe7ee114ce6a052c3db461e
Abhay Kamat <147469947+abhikamat2004@users.noreply.github.com>
2023-12-28 22:51:31 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d %d",&a,&b);     int arr[a][b];     for(int i=0;i<a;i++){         for(int j=0;j<b;j++){         scanf("%d",&arr[i][j]);         }     }     int sum=0;     for(int i=0;i<a;i++){         for(int j=0;j<b;j++){          sum+=arr[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int arr[a][b];
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+        scanf("%d",&arr[i][j]);
+        }
+    }
+    int sum=0;
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+         sum+=arr[i][j];
+        }
+    }
+    printf("%d",sum);
+}
