672089f992db18cbacca5a4e342f608ed5fc29ad
Abhay Kamat <147469947+abhikamat2004@users.noreply.github.com>
2023-12-28 22:09:32 +0530

    #include<stdio.h> #include<string.h> int main() {     char ch[50];     scanf("%[^ ]s",ch);     int c=0;     for(int i=0;i<strlen(ch);i++){         if(ch[i]>='A'&& ch[i]<='Z')c++;     }     printf("%d",c); }

---
+++ Uppercase_Letters_in_a_String.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char ch[50];
+    scanf("%[^
+]s",ch);
+    int c=0;
+    for(int i=0;i<strlen(ch);i++){
+        if(ch[i]>='A'&& ch[i]<='Z')c++;
+    }
+    printf("%d",c);
+}
