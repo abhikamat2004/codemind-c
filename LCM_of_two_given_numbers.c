94575138d8d1a79fb36487a18c7db2288fbb2d42
Abhay Kamat <147469947+abhikamat2004@users.noreply.github.com>
2023-12-31 17:28:40 +0530

    #include<stdio.h> int main() {     int a,b,s;     scanf("%d %d",&a,&b);     int h=1;     if(a<b){         s=a;     }     else{         s=b;     }     for (int i=1;i<=s;i++){         if(a%i==0 && b%i==0){             h=i;         }     }     int l=(a*b)/h;     printf("%d",l); }

--- LCM_of_two_given_numbers.c
+++ LCM_of_two_given_numbers.c
@@ -1,14 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,m;
+    int a,b,s;
     scanf("%d %d",&a,&b);
-    for(int i=1;i<=b;i++){
-        m=a*i;
-        if(m%b==0){
-            printf("%d",m);
-            break;
-            
+    int h=1;
+    if(a<b){
+        s=a;
+    }
+    else{
+        s=b;
+    }
+    for (int i=1;i<=s;i++){
+        if(a%i==0 && b%i==0){
+            h=i;
         }
     }
+    int l=(a*b)/h;
+    printf("%d",l);
 }
